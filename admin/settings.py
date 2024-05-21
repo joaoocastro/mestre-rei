e70bc3d127a30a11451722f5f4de7fea1564a676
riqpedro <pedro.santos13@estudante.ifb.edu.br>
2024-05-20 23:27:49 -0300

    atualizacao home

--- admin/settings.py
+++ admin/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,5 +120,10 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+STATIC_ROOT ='staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
